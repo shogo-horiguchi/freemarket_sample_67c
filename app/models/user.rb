350378162b1f88aea9a33643c721f45b6d586065
mao0923 <60300839+mao0923@users.noreply.github.com>
2020-04-19 04:10:09 +0900

    users_server

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
+  validates :nickname,presence: true,
+                      length: {minimum: 3, maximum: 8}
+  validates :last_name,:first_name,:last_name_kana,:first_name_kana,:birth_date,presence: true
+  has_one :address
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200402041820) do
+ActiveRecord::Schema.define(version: 20200415101025) do
+
+  create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "last_name",       null: false
+    t.string   "first_name",      null: false
+    t.string   "last_name_kana",  null: false
+    t.string   "first_name_kana", null: false
+    t.integer  "postal_code",     null: false
+    t.string   "prefecture",      null: false
+    t.string   "city",            null: false
+    t.string   "street",          null: false
+    t.string   "building"
+    t.integer  "telephone"
+    t.integer  "user_id",         null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "nickname",                            null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "last_name",                           null: false
+    t.string   "first_name",                          null: false
+    t.string   "last_name_kana",                      null: false
+    t.string   "first_name_kana",                     null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "nickname"
+    t.date     "birth_date"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "addresses", "users"
 end
