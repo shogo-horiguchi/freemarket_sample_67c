2356a26fe1f858111a00ee2026e2a9483d9818b8
shogo-h <59693164+shogo-horiguchi@users.noreply.github.com>
2020-04-19 23:27:18 +0900

    delete migration file
    it's because there were double addresses file

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20200412132007) do
-
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "last_name",       null: false
@@ -30,7 +28,6 @@ ActiveRecord::Schema.define(version: 20200412132007) do
     t.datetime "updated_at",      null: false
     t.index ["user_id"], name: "index_addresses_on_user_id", using: :btree
   end
-
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -99,27 +96,27 @@ ActiveRecord::Schema.define(version: 20200412132007) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "nickname",                            null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.string   "last_name",                           null: false
-    t.string   "first_name",                          null: false
-    t.string   "last_name_kana",                      null: false
-    t.string   "first_name_kana",                     null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.date     "birth_date"
-
+    t.string   "nickname"
+    t.string   "last_name",                           null: false
+    t.string   "first_name",                          null: false
+    t.string   "last_name_kana",                      null: false
+    t.string   "first_name_kana",                     null: false
+    t.date     "birth_year",                          null: false
+    t.date     "birth_month",                         null: false
+    t.date     "birth_day",                           null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["nickname"], name: "index_users_on_nickname", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "addresses", "users"
-
   add_foreign_key "comments", "items"
   add_foreign_key "comments", "users"
   add_foreign_key "images", "items"
@@ -128,5 +125,4 @@ ActiveRecord::Schema.define(version: 20200412132007) do
   add_foreign_key "payments", "users"
   add_foreign_key "tags", "categories"
   add_foreign_key "tags", "items"
-
 end
