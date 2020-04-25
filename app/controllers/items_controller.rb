08cf0470f34cf9d36b46300d4156737efafc3e52
takuyatsuji7 <crtseven7717@gmail.com>
2020-04-25 14:55:13 +0900

    item/destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,6 @@ class ItemsController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @item = Item.find(params[:id])
     @brand = @item.brand
     @comment = Comment.new
   end
