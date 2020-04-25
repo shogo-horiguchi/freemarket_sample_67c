dbbc44c1033bca37975094de1d58dff0e96d6b1d
shogo-h <59693164+shogo-horiguchi@users.noreply.github.com>
2020-04-25 21:33:12 +0900

    set basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource) 
@@ -10,6 +10,18 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
+  private
+  
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
@@ -17,7 +29,7 @@ class ApplicationController < ActionController::Base
   end
 
   private
-  
+
   def production?
     Rails.env.production?
   end
