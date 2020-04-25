2dec412383b8bad5b08e96e047a155c0e7d0e468
takuyatsuji7 <crtseven7717@gmail.com>
2020-04-25 14:44:21 +0900

    item/destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :destroy]
+  
   def index
   end
 
@@ -10,8 +12,16 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if current_user == @item.user
+      @item.destroy
+      redirect_to root_path, method: :delete
+    else
+      redirect_to item_path(@item)
+    end
+  end
+
+  private
+  def set_item
     @item = Item.find(params[:id])
-    @item.destroy
-    redirect_to root_path
   end
 end
