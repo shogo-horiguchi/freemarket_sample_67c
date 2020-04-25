a9b80c0f960c1b786416f4520d633011ac0815a2
takuyatsuji7 <crtseven7717@gmail.com>
2020-04-25 14:07:20 +0900

    items/destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-
     @user = User.find(params[:id])
-
     @item = Item.find(params[:id])
     @brand = @item.brand
     @comment = Comment.new
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
   end
 end
