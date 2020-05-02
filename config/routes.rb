03eeb6e2e185a2c68861fe188fbbb9e611ef79ef
mao0923 <60300839+mao0923@users.noreply.github.com>
2020-05-03 00:18:53 +0900

    ユーザー新規登録ログイン1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   root "items#index"
 
   resources :items, only: [:show, :new]
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
   resources :users, only: :show do
 
     collection do
