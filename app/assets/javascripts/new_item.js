eb938cb2a991d26d99f656a899d2db8ae981e56d
hiroyaisaki <60531920+hiroyaisaki@users.noreply.github.com>
2020-05-23 18:02:13 +0900

    商品出品、フロントエンド2回目

--- app/assets/javascripts/new_item.js
+++ app/assets/javascripts/new_item.js
@@ -146,8 +146,6 @@ $(function(){
       .done(function(grandchildren){
         if (grandchildren.length != 0) {
           $('#grandchildren_wrapper').remove();
-          // $('#size_wrapper').remove();
-          // $('#brand_wrapper').remove();
           var insertHTML = '';
           grandchildren.forEach(function(grandchild){
             insertHTML += appendOption(grandchild);
@@ -160,8 +158,6 @@ $(function(){
       })
     }else{
       $('#grandchildren_wrapper').remove();
-      // $('#size_wrapper').remove();
-      // $('#brand_wrapper').remove();
     }
   });
 });
